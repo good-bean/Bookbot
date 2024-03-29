7b3ba2f6cc1e2a2e2c769466c12eaa57a2e7f86f
good-bean <bridget.s.flanery@gmail.com>
2024-03-29 16:43:08 -0700

    add code to read text, add word count function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+def main():
+   book_path = "books/frankenstein.txt"
+   text = get_book_text(book_path)
+   print(word_count(text))
+
+def get_book_text(path):
+   with open(path) as f:
+      return f.read()
+
+def word_count(text):
+   words = len(text.split())
+   return words
+
+main()
