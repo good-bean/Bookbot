6a61f09e6cf58b5914115132511bdf8b8957ce54
good-bean <bridget.s.flanery@gmail.com>
2024-03-29 18:26:02 -0700

    add character frequency counter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
    book_path = "books/frankenstein.txt"
    text = get_book_text(book_path)
-   print(word_count(text))
+   print(word_count(text), "words in Frankenstein")
+   print("The frequency of each character is as follows:", letter_count(text))
 
 def get_book_text(path):
    with open(path) as f:
@@ -10,5 +11,17 @@ def get_book_text(path):
 def word_count(text):
    words = len(text.split())
    return words
+
+def letter_count(text):
+   letters = {}
+   lower_text = text.lower()
+
+   for c in lower_text:
+      if c in letters:
+         letters[c] += 1
+      else:
+         letters[c] = 1
+   
+   return letters
 
 main()
